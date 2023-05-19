cb5f3e91b87098d5b1b6fd75fa36482013523058
Rahul Sarkar <rahulsrkr@gmail.com>
2023-05-19 12:42:58 +0530

    v0.1

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,12 +4,12 @@ import Image from "next/image"
 
 const Footer = () => {
     return (
-        <div className="flex items-center justify-between p-4">
-            <div className="flex items-center gap-4">
+        <div className="flex items-center justify-between p-4 lg:flex-col lg:justify-center lg:gap-4">
+            <div className="flex items-center gap-4  lg:flex-col lg:justify-center lg:gap-4">
                 <Image src={logo} alt="logo" width={150} height={100} />
                 <div className="font-bold">©Shop Sleek Private Ltd. 2023</div>
             </div>
-            <div className="flex items-center gap-12">
+            <div className="flex items-center gap-12  lg:flex-col lg:justify-center lg:gap-4">
                 <div className="flex items-center gap-6">
                     <BsTwitter className="w-5 h-5" />
                     <BsFacebook className="w-5 h-5" />
